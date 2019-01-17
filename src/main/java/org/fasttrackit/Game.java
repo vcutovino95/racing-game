92148a0a8648e58d04091aaa0122ee60720d304b
Cutovino Valentin <valentin.cutovino@yahoo.com>
2019-01-17 20:34:15 +0200

    studied for loops

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -3,13 +3,13 @@ package org.fasttrackit;
 public class Game {
 
 
-    private Track[] tracks =  new Track[10];
+    private Track[] tracks = new Track[10];
 
     Vehicle firstCompetitor;
     Vehicle secondCompetitor;
 
 
-    public void start () {
+    public void start() {
 
 
         addTracks();
@@ -17,35 +17,51 @@ public class Game {
     }
 
 
-    private void addTracks () {
+    private void addTracks() {
 
 
         Track track1 = new Track("Highway", 300);
         Track track2 = new Track("Seaside", 100);
 
 
-        tracks [0] = track1;
-        tracks [1] = track2;
-
-
-
-
+        tracks[0] = track1;
+        tracks[1] = track2;
 
 
     }
 
 
-    private void displayAvailableTrack(){
+    private void displayAvailableTrack() {
 
 
         System.out.println("Available tracks");
-        System.out.println(tracks [0].getName());
-        System.out.println(tracks [1].getName());
+        System.out.println(tracks[0].getName());
+        System.out.println(tracks[1].getName());
+
+
+        // classic for loop
+//        for (int i = 0; i < tracks.length; i++) {
+//
+//
+//            if (tracks[i] != null) {
+//                System.out.println(tracks[i].getName());
+//
+//
+//            }
+//
+//
+//        }
+
+
+        // enhanced for / "for-each"
+        for (Track track : tracks) {
+            if (track != null) {
+                System.out.println(track.getName());
 
 
 
-
-
+            }
+        }
 
     }
 
